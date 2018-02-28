dead2a63f4b92b340fdd85b90016d13ab026aac3
Robert Gerboth <email@example.com>
2018-02-28 16:06:50 -0700

    initial updload

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,42 +1,119 @@
-// Require dependencies
-var http = require("http");
-var fs = require("fs");
+// Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
 
-// Set our port to 8080
-var PORT = 8080;
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
 
-var server = http.createServer(handleRequest);
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-function handleRequest(req, res) {
-
-  // Capture the url the request is made to
-  var path = req.url;
-
-  // When we visit different urls, read and respond with different files
-  switch (path) {
-
-    case "/survey":
-      return fs.readFile(__dirname + "./public/survey.html", function(err, data) {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(data);
-      });
-
-    case "/github":
-      return fs.readFile("https://github.com/rgerboth/friendfinder", function(err, data) {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(data);
-      });
-
-    // default to rendering home.html, if none of above cases are hit
-    default:
-      return fs.readFile(__dirname + "./public/home.html", function(err, data) {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(data);
-      });
+//Seed data for friends list
+var friends = [
+  {
+    routeName: "daenerystargaryen",
+    name: "Daenerys Targaryen",
+    image: "https://vignette.wikia.nocookie.net/gameofthrones/images/5/5f/Daenerys_Dragonpit.jpg/revision/latest?cb=20171015095128",
+    scores: ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1"]
+  },
+  {
+    routeName: "jonsnow",
+    name: "John Snow",
+    image: "https://vignette.wikia.nocookie.net/gameofthrones/images/1/17/Jon-Snow-Kit-Harington_510.jpeg/revision/latest/scale-to-width-down/2000?cb=20120223002142",
+    scores: ["3", "3", "3", "3", "3", "3", "3", "3", "3", "3"]
+  },
+  {
+    routeName: "tyrionlannister",
+    name: "Tyrion Lannister",
+    image: "https://upload.wikimedia.org/wikipedia/en/thumb/5/50/Tyrion_Lannister-Peter_Dinklage.jpg/220px-Tyrion_Lannister-Peter_Dinklage.jpg",
+    scores: ["5", "5", "5", "5", "5", "5", "5", "5", "5", "5"]
   }
-}
+];
 
-// Starts our server.
-server.listen(PORT, function() {
-  console.log("Server is listening on PORT: " + PORT);
+// Routes
+// =============================================================
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "../public/home.html"));
+});
+
+app.get("/survey", function(req, res) {
+  res.sendFile(path.join(__dirname, "../public/survey.html"))
+})
+
+app.post("/api/findFriend", function(req, res) {
+    var newFriend = req.body;
+    newFriend.scores = newFriend["scores[]"]
+    newFriend["scores[]"] = []
+    console.log(newFriend)
+    console.log(newFriend.routeName + newFriend.name + newFriend.image);
+    console.log(newFriend);
+        // console.log(newFriend.routeName + newFriend.name + newFriend.image + newFriend.scores[0])
+    if (newFriend) {
+
+      var lowFriendScore = 40
+      var newFriendScore = 0
+      var newBestieIndex = 0
+      //loop through friends[] and add up difference
+      //lowest difference is the match     
+      // 
+      for (i=0; i<friends.length; i++){
+        for (j=0; j<10; j++) {
+          console.log("Friend: " + friends[i].scores[j])
+          console.log("New:    " + newFriend.scores[j])
+          newFriendScore += Math.abs(friends[i].scores[j] - newFriend.scores[j])
+        }
+          console.log("New Friend Score: " + newFriendScore)
+          console.log("Low Friend Score: " + lowFriendScore)
+        if (newFriendScore <= lowFriendScore) {
+          lowFriendScore = newFriendScore
+          newFriendScore = 0
+          newBestieIndex = i
+          console.log("New Bestie Index: " + newBestieIndex)
+        } 
+
+        newFriendScore = 0
+
+      }
+      //add the new friend to the list and return the best match to display
+      newFriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
+      friends.push(newFriend)
+
+      return res.json(friends[newBestieIndex])
+
+    }
+
+    return res.json(friends);
+    
+});
+
+app.get("/api", function(req, res) {
+    return res.json(friends);
+});
+
+// Create New Characters - takes in JSON input
+app.post("/api/new", function(req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body-parser middleware
+  var newcharacter = req.body;
+  // Using a RegEx Pattern to remove spaces from newCharacter
+  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+  newcharacter.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
+
+  console.log(newcharacter);
+
+  friends.push(newcharacter);
+
+  res.json(newcharacter);
+});
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
 });
