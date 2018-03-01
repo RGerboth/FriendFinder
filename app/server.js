90465dfb8b794261599c92b16f8b8f69c62f9765
Robert Gerboth <email@example.com>
2018-03-01 08:01:31 -0700

    Updates to UI and friends array

--- app/server.js
+++ app/server.js
@@ -19,19 +19,43 @@ var friends = [
     routeName: "daenerystargaryen",
     name: "Daenerys Targaryen",
     image: "https://vignette.wikia.nocookie.net/gameofthrones/images/5/5f/Daenerys_Dragonpit.jpg/revision/latest?cb=20171015095128",
-    scores: ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1"]
+    scores: ["5", "2", "3", "4", "4", "4", "2", "3", "3", "2"]
   },
   {
     routeName: "jonsnow",
     name: "John Snow",
-    image: "https://vignette.wikia.nocookie.net/gameofthrones/images/1/17/Jon-Snow-Kit-Harington_510.jpeg/revision/latest/scale-to-width-down/2000?cb=20120223002142",
+    image: "https://vignette.wikia.nocookie.net/gameofthrones/images/a/a5/Profile-JonSnow-707.png/revision/latest?cb=20170828030553",
+    scores: ["2", "4", "4", "5", "4", "3", "5", "4", "3", "2"]
+  },
+  {
+    routeName: "cerseilannister",
+    name: "Cersei Lannister",
+    image: "https://vignette.wikia.nocookie.net/gameofthrones/images/8/87/Cersei_Lannister_%28S05E01%29.jpg/revision/latest?cb=20150420193418",
+    scores: ["4", "2", "4", "5", "1", "5", "4", "1", "2", "3"]
+  },
+  {
+    routeName: "aryastark",
+    name: "Arya Stark",
+    image: "https://vignette.wikia.nocookie.net/gameofthrones/images/5/54/Arya_the_dragon_and_the_wolf_s7.jpg/revision/latest/scale-to-width-down/319?cb=20170828062911",
+    scores: ["5", "4", "5", "2", "3", "2", "4", "3", "4", "2"]
+  },
+  {
+    routeName: "margaerytyrell",
+    name: "Margaery Tyrell",
+    image: "https://vignette.wikia.nocookie.net/gameofthrones/images/b/b5/Margaery_Tyrell_S6.png/revision/latest?cb=20160729224605",
+    scores: ["5", "4", "4", "3", "4", "2", "1", "4", "5", "5"]
+  },
+  {
+    routeName: "robert",
+    name: "Robert",
+    image: "https://vignette.wikia.nocookie.net/gameofthrones/images/b/bc/Sandor_Clegane_Season_7_Promo_Image.PNG/revision/latest/scale-to-width-down/343?cb=20170828063713",
     scores: ["3", "3", "3", "3", "3", "3", "3", "3", "3", "3"]
   },
   {
     routeName: "tyrionlannister",
     name: "Tyrion Lannister",
     image: "https://upload.wikimedia.org/wikipedia/en/thumb/5/50/Tyrion_Lannister-Peter_Dinklage.jpg/220px-Tyrion_Lannister-Peter_Dinklage.jpg",
-    scores: ["5", "5", "5", "5", "5", "5", "5", "5", "5", "5"]
+    scores: ["5", "2", "2", "1", "4", "3", "5", "3", "2", "1"]
   }
 ];
 
@@ -50,10 +74,7 @@ app.post("/api/findFriend", function(req, res) {
     var newFriend = req.body;
     newFriend.scores = newFriend["scores[]"]
     newFriend["scores[]"] = []
-    console.log(newFriend)
-    console.log(newFriend.routeName + newFriend.name + newFriend.image);
-    console.log(newFriend);
-        // console.log(newFriend.routeName + newFriend.name + newFriend.image + newFriend.scores[0])
+
     if (newFriend) {
 
       var lowFriendScore = 40
@@ -64,8 +85,6 @@ app.post("/api/findFriend", function(req, res) {
       // 
       for (i=0; i<friends.length; i++){
         for (j=0; j<10; j++) {
-          console.log("Friend: " + friends[i].scores[j])
-          console.log("New:    " + newFriend.scores[j])
           newFriendScore += Math.abs(friends[i].scores[j] - newFriend.scores[j])
         }
           console.log("New Friend Score: " + newFriendScore)
